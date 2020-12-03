ae6f1c215b6c94ee6d44c8d30859087194b18bae
Lymeng <Lymengchun@gmail.com>
2020-12-03 17:01:42 +0700

    jfa

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function conver(){
+
+    let valNum = document.getElementById('inputNumber').value;
+    let valResult = document.getElementById('Result');
+
+    valResult.value = valNum;
+    console.log(valNum.value);
+
+} 
